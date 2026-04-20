4bbb260ee97c59bcd2c3851e81ff0e00dbf38c3d
Oshen Vithanage <lagmomy@gmail.com>
2026-04-20 19:08:44 +0530

    Day23✏️

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { authenticateUser } from "./middlewares/authentication.js";
 import productRouter from "./routers/productRouter.js";
 import cors from "cors"
 import dotenv from "dotenv"
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config()
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(authenticateUser);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(port, () => {
   console.log("Server is runnign on port: " + port + "");
